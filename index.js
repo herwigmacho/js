fa285508ffe3d4fc1b22ff22b1dba3c4ae4344a1
DI Herwig Macho <h.macho@htlkrems.ac.at>
2022-02-20 08:07:35 +0000

    extra ohm program

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 console.log('Hello, world!')
-/*
-var input = prompt("What's your name?");
-alert(`Your name is ${input}`);
-var r = prompt("R=")
-var u = prompt("U=")
-i=u/r
-console.log("I="+i)
-*/
+
 const bound=10
 mainloop()
 
---
+++ ohm.js
@@ -0,0 +1,6 @@
+var input = prompt("What's your name?");
+alert(`Your name is ${input}`);
+var r = prompt("R=")
+var u = prompt("U=")
+i=u/r
+console.log("I="+i)
